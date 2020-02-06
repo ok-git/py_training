9fec04bebc5eeb8e1c935275bc5e2bd68fffcd1a
Oleg <olka.contact@gmail.com>
2020-02-06 20:34:45 +0700

    Готово task_1

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -1,1 +1,20 @@
-sec = input("Input time in seconds:")
+sec = ''  # define var
+
+# waiting for digits only
+while not sec.isdigit():
+    sec = input("Input time in seconds:")
+sec = int(sec)  # change str to int
+
+# variant 1
+hours = sec // 3600
+minutes = sec // 60 % 60
+seconds = sec % 60
+
+print(f"v1 {sec} seconds is equal to {hours} hour(s), {minutes} minute(s) and {seconds} second(s) ")
+
+# variant 2
+hours = sec // 3600
+minutes = sec % 3600 // 60
+seconds = sec % 3600 % 60
+
+print(f"v2 {sec} seconds is equal to {hours} hour(s), {minutes} minute(s) and {seconds} second(s) ")
