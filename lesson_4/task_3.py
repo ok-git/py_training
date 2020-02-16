86be355c9797b8b7ee894ae82f018d999f851638
ok-git <okmail.inbox@gmail.com>
2020-02-16 22:14:04 +0700

    Подправил lesson_4.task_3

--- lesson_4/task_3.py
+++ lesson_4/task_3.py
@@ -3,4 +3,4 @@
 Подсказка: использовать функцию range() и генератор.
 """
 
-print([el for el in range(20, 240) if el % 20 == 0 or el % 21 == 0])
+print(*[el for el in range(20, 240) if el % 20 == 0 or el % 21 == 0])
