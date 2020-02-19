808577fdced50ed26a6c52cdc31add9d7b669e95
Oleg <olka.contact@gmail.com>
2020-02-19 20:08:33 +0700

    Сделал lesson_5.task_3

---
+++ lesson_5/task_3.py
@@ -0,0 +1,18 @@
+"""
+Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов (не менее 10
+строк). Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет
+средней величины дохода сотрудников.
+"""
+
+staff_list = []
+salary_list = []
+with open("staff.txt", 'r', encoding='utf-8') as f_obj:
+    for line in f_obj:
+        person, salary = line.split()
+        staff_list.append(person)
+        salary_list.append(float(salary))
+
+under20000 = [person for person, salary in zip(staff_list, salary_list) if salary < 20000]
+print('Оклады до 20000:', *under20000)
+
+print(f'Средний оклад: {sum(salary_list)/len(salary_list):.2f} \n')
